25361d717ecf1677fd208561e8c29c1b0fefdc33
nontachai <pappaliving6395@gmail.com>
2024-11-24 21:23:26 +0700

    fix error removing pre-service card

--- components/Frontend/TabbedSection.tsx
+++ components/Frontend/TabbedSection.tsx
@@ -26,21 +26,3 @@ const TabbedSection: React.FC = () => {
 };
 
 export default TabbedSection;
-
-const ServiceCard = ({ icon, title, details }) => {
-  return (
-    <>
-      <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-        <div className="mb-9 rounded-[20px] bg-white p-10 shadow-2 hover:shadow-lg dark:bg-dark-2 md:px-7 xl:px-10">
-          <div className="mb-8 flex h-[70px] w-[70px] items-center justify-center rounded-2xl bg-primary">
-            {icon}
-          </div>
-          <h4 className="mb-[14px] text-2xl font-semibold text-dark dark:text-white">
-            {title}
-          </h4>
-          <p className="text-body-color dark:text-dark-6">{details}</p>
-        </div>
-      </div>
-    </>
-  );
-};
