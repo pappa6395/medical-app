31b6a5fd8be60913d628f9d92fbea5f71d5afbc8
nontachai <pappaliving6395@gmail.com>
2024-11-27 22:34:19 +0700

    id type changed to any in verify page

--- app/(front)/verifyAccount/page.tsx
+++ app/(front)/verifyAccount/page.tsx
@@ -4,7 +4,7 @@ import VerifyTokenForm from "@/components/Frontend/VerifyTokenForm";
 export default async function VerifyAccount({
   params: { id },
 }: {
-  params: { id: string };
+  params: { id: any };
 }) {
   //Get a User fromm DB
   const user = await getUserById(id);
