29ba8dcf6d376225a30cc27d7ec300e7a3c61fb3
nontachai <pappaliving6395@gmail.com>
2025-01-04 22:12:31 +0700

    Check 3 roles

--- app/(back)/dashboard/page.tsx
+++ app/(back)/dashboard/page.tsx
@@ -19,9 +19,9 @@ const page = async() => {
   const userId = user?.id??""
   const role = user?.role;
 
-  const doctorAnalytics = await getDoctorAnalytics() || []
+  //const doctorAnalytics = await getDoctorAnalytics() || []
   // const userAnalytics = await getUserAnalytics() || []
-  const analytics = await getAdminAnalytics();
+  //const analytics = await getAdminAnalytics();
   
   // for doctors, get recent appointment by doctor id and get recent patients from appointment map patient id
   
@@ -30,8 +30,8 @@ const page = async() => {
   // // Option 2 : [patientId, name, email] => remove dups
   //const recentAppointments = (await getAppointments())?.data || []
   //const doctors = await getDoctorsById(userId)
-  const doctorsAdmin = await getDoctors() || [] as Doctor[]
-  const appointmentsAdmin = (await getAppointments()).data || []
+  //const doctorsAdmin = await getDoctors() || [] as Doctor[]
+  //const appointmentsAdmin = (await getAppointments()).data || []
     
   //for User, Recent get doctor by patientId and Recent appointment  by patientId
   // const appointmentByPatientId = (await getAppointmentByPatientId(userId))?.data || [];
@@ -50,42 +50,45 @@ const page = async() => {
       
   //----------------------------------------------------------------//
 
-  // if (role === "DOCTOR") {
-  //   return (
-  //     <div>
-  //       <DoctorDashboard 
-  //         session={session} 
-  //         analytics={doctorAnalytics}
-  //         patientsApp={appointments} 
-  //         doctors={doctors}
-  //         appointments={recentAppointments}
-  //       />
-  //     </div>
-  //   );
-  // }
-  
-  // if (role === "USER") {
-  //   return (
-  //     <div>
-  //       <PatientDashboard 
-  //         session={session} 
-  //         analytics={userAnalytics} 
-  //         doctors={doctorsPatientId}
-  //         appointments={appointmentByPatientId}
-  //         role={role} 
-  //       />
-  //     </div>
-  //   )
-  // }
+  if (role === "DOCTOR") {
+    return (
+      <div>
+        <h2>I am a Doctor</h2>
+        {/* <DoctorDashboard 
+          session={session} 
+          analytics={doctorAnalytics}
+          patientsApp={appointments} 
+          doctors={doctors}
+          appointments={recentAppointments}
+        /> */}
+      </div>
+    );
+  }
+
+  if (role === "USER") {
+    return (
+      <div>
+        <h2>I am a Patient</h2>
+        {/* <PatientDashboard 
+          session={session} 
+          analytics={userAnalytics} 
+          doctors={doctorsPatientId}
+          appointments={appointmentByPatientId}
+          role={role} 
+        /> */}
+      </div>
+    )
+  }
 
   return (
     <div>
-      <Dashboard
+      <h2>I am an Admin</h2>
+      {/* <Dashboard
         session={session}
         analytics={analytics}
         doctors={doctorsAdmin}
         appointments={appointmentsAdmin}
-      />
+      /> */}
     </div>
   )
 }
